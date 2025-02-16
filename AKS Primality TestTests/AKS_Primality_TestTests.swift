775de2548346417c21a18a95c2f854f2fc72c524
aayushkumar3676 <aayushkumarjsr16@protonmail.com>
2025-02-16 23:46:38 +0530

    AKS

---
+++ AKS Primality TestTests/AKS_Primality_TestTests.swift
@@ -0,0 +1,17 @@
+//
+//  AKS_Primality_TestTests.swift
+//  AKS Primality TestTests
+//
+//  Created by Aayush kumar on 16/02/25.
+//
+
+import Testing
+@testable import AKS_Primality_Test
+
+struct AKS_Primality_TestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
